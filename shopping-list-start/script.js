6b7866b7abeff6c519194345b4978a9f764ce937
CelicMilos <165153757+CelicMilos@users.noreply.github.com>
2025-01-17 11:08:56 +0100

    Update script.js
    Clearing and filtering function added.

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -3,6 +3,7 @@ const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 const header = document.getElementById("title");
 const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -19,7 +20,9 @@ function addItem(e) {
     }, 1500);
     return;
   } else {
-    header.textContent = `${newItem}!Verry nice!`;
+    header.textContent = `${
+      newItem[0].toUpperCase() + newItem.slice(1)
+    }!Verry nice!`;
     header.style.color = "#ab2e26";
     setTimeout(() => {
       header.textContent = "Shopping list";
@@ -29,10 +32,13 @@ function addItem(e) {
 
   //   Create list item
   const li = document.createElement("li");
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(
+    document.createTextNode(newItem[0].toUpperCase() + newItem.slice(1))
+  );
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
+  resetUI();
   itemInput.value = "";
 }
 // Create button
@@ -54,6 +60,7 @@ function removeItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
     e.target.parentElement.parentElement.remove();
   }
+  resetUI();
 }
 function clearItems(e) {
   //   itemList.innerHTML = "";
@@ -61,10 +68,38 @@ function clearItems(e) {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  resetUI();
+}
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    // .indexOf prolazi kroz sve clanove niza (array),uporedjuje ih i ako nadje
+    // match onda ce biti true,ako ne nadje onda izbacuje -1
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
+function resetUI() {
+  const items = itemList.querySelectorAll("li");
+  // console.log(items.length);
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
 // Event Listeners
 
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+resetUI();
